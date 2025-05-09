8eaf949b5ecdfb9d8840f1712cf382480bfe483c
CSReviser <46049273+CSReviser@users.noreply.github.com>
2025-05-09 15:12:55 +0900

    MainActivity.java を更新

--- app/src/main/java/com/github/naofum/gogakudroid/MainActivity.java
+++ app/src/main/java/com/github/naofum/gogakudroid/MainActivity.java
@@ -33,6 +33,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
